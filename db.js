523539d8a05ee51fc7b06b1cbbdabcbeb5dc9b96
Navya Raturi <codewithnavya@proton.me>
2025-10-17 16:00:44 +0000

    add schema definatoins and connection

--- db.js
+++ db.js
@@ -1,17 +1,50 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
+
+mongoose.connect(BUN.env.MONGODB_CONNECTION_STRING); // TODO: Add connection string
 
 const ObjectId = Schema.Types.ObjectId;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   name: String,
+  password: String,
 });
 
-const adminSchema = new Schema({});
-const courseSchema = new Schema({});
-const purchaseSchema = new Schema({});
+const adminSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  name: String,
+  password: String,
+});
+
+const courseSchema = new Schema({
+  creatorId: {
+    type: ObjectId,
+    ref: "admin",
+  },
+  description: String,
+  imageUrl: String,
+  price: Number,
+  title: {
+    type: String,
+    unique: true,
+  },
+});
+const purchaseSchema = new Schema({
+  userId: {
+    type: ObjectId,
+    ref: "users",
+  },
+  courseId: {
+    type: ObjectId,
+    ref: "courses",
+  },
+});
 
 const Admin = model("admin", adminSchema);
 const Purchases = model("purchases", purchaseSchema);
