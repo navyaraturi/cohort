16076d5464865ae7f97b5cb073e082cf15a59c04
Navya Raturi <codewithnavya@proton.me>
2025-10-17 13:57:23 +0000

    add route placeholder for different endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(
   cors({
     origin: "*",
@@ -11,6 +12,12 @@ app.use(
 app.get("/", (req, res) => {
   return res.send("Cohort app");
 });
+
+app.post("/signup", (req, res) => {});
+app.post("/signin", (req, res) => {});
+app.post("/purchase-course", (req, res) => {});
+app.get("/courses", (req, res) => {});
+app.get("/purchased-courses", (req, res) => {});
 
 app.listen(Bun.env.PORT, () => {
   console.log(`Server listening on ${Bun.env.PORT}`);
