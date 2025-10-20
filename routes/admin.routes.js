651ac553b96c3d36d69e3a0f2cd3efec097e8fd6
Navya Raturi <codewithnavya@proton.me>
2025-10-20 14:44:24 +0000

    add constraints for admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -27,16 +27,34 @@ router.post("/signin", (req, res) => {
 
 // create a new course
 router.post("/course", (req, res) => {
+  /* constraint: is a protected route, needs to be admin
+   * req.body get `title`, `image`, `content` & `price` which is ideally some text
+   * zod validate
+   * save the course to the database
+   * handle course title already present error, mongoose should by itself check that
+   * return the new course id to the user
+   */
   return res.send("create a new course");
 });
 
 // update a course
 router.post("/course", (req, res) => {
+  /* constraint: is a protected route, needs to be admin
+   * req.body get `title`, `image`, `content` & `price` which is ideally some text
+   * zod validate to include course name and image or content or price or both
+   * update the course in the database
+   * think if the course is not present return valid error message
+   * return the updated course id to the user
+   */
   return res.send("update a given course");
 });
 
 // get all the courses created by the user
-router.post("/course/list", (req, res) => {
+router.get("/course/list", (req, res) => {
+  /* constraint: is a protected route, needs to be admin
+   * fetch all the courses of the user from the database
+   * return the courses back to the user
+   */
   return res.send("here are all the courses you created");
 });
 
